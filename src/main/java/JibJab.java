feeb9d418d2b559802d101caf6ee06f456bedde9
niyniy123 <177832011+niyniy123@users.noreply.github.com>
2025-08-17 13:19:07 +0800

    Add support for listing of user inputs

--- src/main/java/JibJab.java
+++ src/main/java/JibJab.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JibJab {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> userInputs = new ArrayList<>();
 
         System.out.println("Hello from JibJab");
         System.out.println("What can I do for you?");
@@ -13,6 +15,13 @@ public class JibJab {
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
+            } else if (userInput.equals("list")) {
+                int counter = 1;
+
+                for (String input : userInputs) {
+                    System.out.println(counter + ". " + input);
+                    counter++;
+                }
             } else {
                 userInputs.add(userInput);
                 System.out.println("Added: " + userInput);
