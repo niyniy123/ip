dbccb5240f3d3cc4c390bd45736febcdd388e157
niyniy123 <177832011+niyniy123@users.noreply.github.com>
2025-08-17 14:02:12 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,17 @@
+public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public void setNotDone() {
+        this.isDone = false;
+    }
+}
