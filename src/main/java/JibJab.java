8d20941021cca77aaca902e4df23a7ab2350fa09
niyniy123 <177832011+niyniy123@users.noreply.github.com>
2025-08-17 13:04:23 +0800

    Add support for echo and exit command

--- src/main/java/JibJab.java
+++ src/main/java/JibJab.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class JibJab {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello from JibJab");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                System.out.println(userInput);
+            }
+        }
+
     }
 }
