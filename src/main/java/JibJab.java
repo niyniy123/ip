5ef5de2903bbd7c3152dd17c6d1dcdac7b28f187
user <177832011+niyniy123@users.noreply.github.com>
2025-08-24 12:54:13 +0800

    Add support for saving and loading tasks to/from local file

--- src/main/java/JibJab.java
+++ src/main/java/JibJab.java
@@ -1,11 +1,76 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JibJab {
     private ArrayList<Task> tasks;
+    private Scanner sc;
+
 
     public JibJab() {
         this.tasks = new ArrayList<>();
+        this.loadTasks();
+    }
+
+    public void saveTasks() {
+        try {
+            PrintWriter pw = new PrintWriter("data/jibjab.txt");
+            for (Task task : this.tasks) {
+                pw.println(task.toString());
+            }
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadTasks() {
+        String path = "data/jibjab.txt";
+        File file = new File(path);
+        if (file.exists()) {
+            try {
+                sc = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String taskType = Character.toString(line.charAt(1));
+                String taskStatus =  Character.toString(line.charAt(4));
+                String taskDesc = line.substring(7);
+
+                if (taskType.equals("T")) {
+                    ToDo task = new ToDo(taskDesc);
+                    if (taskStatus.equals("X")) {
+                        task.setDone();
+                    }
+                    this.tasks.add(task);
+                } else if (taskType.equals("D")) {
+                    String[] split = taskDesc.split("by: ");
+                    String taskBy = split[1].substring(0, split[1].indexOf(")"));
+                    Deadline task = new Deadline(taskDesc, taskBy);
+                    if (taskStatus.equals("X")) {
+                        task.setDone();
+                    }
+                    this.tasks.add(task);
+                } else if (taskType.equals("E")) {
+                    String[] split = taskDesc.split("from: ");
+                    String[] fromTo = split[1].split(" to: ");
+                    Event task = new Event(taskDesc, fromTo[0], fromTo[1]);
+                    if (taskStatus.equals("X")) {
+                        task.setDone();
+                    }
+                    this.tasks.add(task);
+                } else {
+                    System.err.println("Unknown task type: " + taskType);
+                }
+            }
+        }
     }
 
     public void addTask(Task task) {
@@ -13,6 +78,7 @@ public class JibJab {
         System.out.println("Got it. I've added this task:");
         System.out.println(task);
         System.out.println("Now you have " + this.tasks.size() + " tasks in the list");
+        this.saveTasks();
     }
 
     public void deleteTask(int idx) {
@@ -21,18 +87,21 @@ public class JibJab {
         System.out.println("Noted. I've removed this task:");
         System.out.println(task);
         System.out.println("Now you have " + this.tasks.size() + " tasks in the list");
+        this.saveTasks();
     }
 
     public void markTaskAsDone(int idx) {
         Task task = this.tasks.get(idx);
         task.setDone();
         System.out.println("Nice! I've marked this task as done:\n" + task);
+        this.saveTasks();
     }
 
     public void markTaskAsNotDone(int idx) {
         Task task = this.tasks.get(idx);
         task.setNotDone();
         System.out.println("OK, I've marked this task as not done yet:\n" + task);
+        this.saveTasks();
     }
 
     public void listTasks() {
@@ -59,6 +128,7 @@ public class JibJab {
 
             try {
                 if (firstWord.equals("bye")) {
+                    jj.saveTasks();
                     System.out.println("Bye. Hope to see you again soon!");
                     return;
                 } else if (firstWord.equals("todo")) {
