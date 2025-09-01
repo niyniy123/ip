48d273c50ed8e8389feacb87770a650b3194b7e6
niyniy123 <177832011+niyniy123@users.noreply.github.com>
2025-09-01 12:14:26 +0800

    Add more JavaFX files

--- src/main/java/jibjab/MainWindow.java
+++ src/main/java/jibjab/MainWindow.java
@@ -1,4 +1,54 @@
 package jibjab;
 
-public class MainWindow {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private JibJab jibjab;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/man.png"));
+    private Image jibjabImage = new Image(this.getClass().getResourceAsStream("/images/ai.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+
+
+    }
+
+    /** Injects the JibJab instance */
+    public void setJibJab(JibJab j) {
+        jibjab = j;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = jibjab.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, jibjabImage)
+        );
+        userInput.clear();
+    }
 }
